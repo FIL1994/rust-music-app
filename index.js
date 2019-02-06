f66d0292a8fec8a4aa205780d47a2ac7f5cc7e1f
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-02-05 22:53:08 -0500

    create db, create table, insert data and select data

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const sqlite3 = require("sqlite3").verbose();
+
+// will create db if it doesn't exist
+let db = new sqlite3.Database("./mydb.db", err => {
+  if (err) {
+    console.log("error", err.message);
+  } else {
+    console.log("Connected to db");
+  }
+});
+
+db.run("CREATE TABLE IF NOT EXISTS langs(name text)");
+db.run("INSERT INTO langs(name) VALUES(?)", ["test"]);
+db.all("SELECT * FROM langs", [], (err, rows) => {
+  console.log("db", rows);
+});
+
+db.close(err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Closed the database connection.");
+});
+
 const addon = require("./native");
 const { app, BrowserWindow, dialog, ipcMain } = require("electron");
 const contextMenu = require("electron-context-menu");
 const mm = require("music-metadata");
-
-console.log("V", Number(process.version.match(/^v(\d+\.\d+)/)[1]));
 
 const {
   CHOOSE_FILE,
